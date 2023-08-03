5860557d6176cae664d64e89b68a23837ff66ee9
seongjju <95429101+seongjju@users.noreply.github.com>
2023-08-03 20:43:11 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 108 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.py
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.py
@@ -1,9 +1,9 @@
 n=int(input())
 arr=list(map(int,input().split()))
 
-prefix_sum=[arr[0]]
+prefix_sum=[0]*n
 for i in range(1,n):
-    prefix_sum.append(prefix_sum[i-1]+arr[i])
+    prefix_sum[i]=prefix_sum[i-1]+arr[i]
 
 cnt=0
 for i in range(n-1):
