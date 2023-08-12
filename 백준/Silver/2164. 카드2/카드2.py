d0f90a1c1850ae8fd7e4a236115c68f6d03ce3d3
seongjju <95429101+seongjju@users.noreply.github.com>
2023-08-12 15:51:59 +0900

    [Silver IV] Title: 카드2, Time: 572 ms, Memory: 51068 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):          # 빈 큐 하나를 생성합니다.
+        self.dq = deque()
+                
+    def push(self, item):        # 큐의 맨 뒤에 데이터를 추가합니다.
+        self.dq.append(item)
+                
+    def empty(self):             # 큐가 비어있으면 True를 반환합니다.
+        return not self.dq
+                
+    def size(self):              # 큐에 들어있는 데이터 수를 반환합니다.
+        return len(self.dq)
+        
+    def pop(self):               # 큐의 맨 앞에 있는 데이터를 반환하고 제거합니다.
+        if self.empty():
+            raise Exception("Queue is empty")
+            
+        return self.dq.popleft()
+                
+    def front(self):             # 큐의 맨 앞에 있는 데이터를 제거하지 않고 반환합니다.
+        if self.empty():
+            raise Exception("Queue is empty")
+                        
+        return self.dq[0]
+
+
+q = Queue()          # 정수를 관리할 stack을 선언합니다. => 빈 스택
+n=int(input())
+cnt=0
+for i in range(1,n+1):
+    q.push(i)
+while q.size()!=1:
+    cnt+=1
+    if cnt%2==1:
+        q.pop()
+    else:
+        x=q.front()
+        q.push(x)
+        q.pop()
+print(q.front())
